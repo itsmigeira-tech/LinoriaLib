c9d35c2a1cd560b2eea32715154b02920e1fd21e
itsmigeira-tech <itsmigeira@gmail.com>
2026-09-17 18:36:15 -0400

    docs: match example to thinner outlines

--- Example.lua
+++ Example.lua
@@ -11,6 +11,7 @@ local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua' .. 
 local Toggles = Library.Toggles
 local Options = Library.Options
 
+Library.Black = Color3.fromRGB(0, 0, 0)
 Library.OutlineColor = Library.Black
 
 local Window = Library:CreateWindow({
@@ -18,7 +19,7 @@ local Window = Library:CreateWindow({
     Center = true,
     AutoShow = true,
     Size = UDim2.fromOffset(560, 500),
-    TabPadding = 1,
+    TabPadding = 2,
     MenuFadeTime = 0.12
 })
 
